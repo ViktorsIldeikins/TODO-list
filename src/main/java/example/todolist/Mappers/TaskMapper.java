9eabb9596eab7a880d3a860933d0bd38992cb0ef
Ildeikins <viktors.ildeikins@accenture.com>
2017-12-07 15:46:13 +0200

    fix db create table error

--- src/main/java/example/todolist/Mappers/TaskMapper.java
+++ src/main/java/example/todolist/Mappers/TaskMapper.java
@@ -28,12 +28,12 @@ public interface TaskMapper {
 
 	@Insert("CREATE TABLE IF Not EXISTS Tasks\n" +
 			"(\n" +
-			"    id int(10) NOT NULL AUTO_INCREMENT,\n" +
-			"    person varchar(100) NOT NULL,\n" +
-			"    task VARCHAR (100) NOT NULL,\n" +
-			"    amountOfCoffeeCups int(10) DEFAULT 0,\n" +
-			"    usedCoffeeCups int(10) DEFAULT 0,\n" +
-			"    PRIMARY KEY (id)\n" +
+			"    id int[10] NOT NULL AUTO_INCREMENT,\n" +
+			"    person varchar[100] NOT NULL,\n" +
+			"    task VARCHAR [100] NOT NULL,\n" +
+			"    amountOfCoffeeCups int[10] DEFAULT 0,\n" +
+			"    usedCoffeeCups int[10] DEFAULT 0,\n" +
+			"    PRIMARY KEY [id]\n" +
 			");")
 	void createTable();
 
